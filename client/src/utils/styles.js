12376d5d307c4c5f3ceed079de41ac8637267b22
lee-shannon <96852559+lee-shannon@users.noreply.github.com>
2023-05-09 14:24:50 -0600

    Create styles.js

---
+++ client/src/utils/styles.js
@@ -0,0 +1,41 @@
+export const goldenContainerStyle = {
+    background: '#B7B78A',
+    margin: '0.2vw',
+    padding: '0.5vw',
+}
+
+export const grayContainerStyle = {
+    background: '#DDDDDD',
+    margin: '0.2vw',
+    padding: '0.2vw',
+}
+
+export const darkGrayContainerStyle = {
+    background: '#CCCCCC',
+}
+
+export const pageStyle = {
+    padding: '1vw',
+}
+
+export const redText = {
+    color: '#eb3434'
+}
+
+export const greenText = {
+    color: '#49b51f'
+}
+
+export const inlineButton =  {
+    margin: 5,
+    display: 'flex'
+}
+
+export const inputTextStyle ={
+    marginLeft: "10px",
+    padding: "5px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    width: '15vw',
+    fontsize: 100
+}
