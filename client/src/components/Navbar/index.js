8f21231c96c0d653ed5a3437cba39f76aaf17fee
lee-shannon <96852559+lee-shannon@users.noreply.github.com>
2023-05-09 14:18:45 -0600

    Create index.js

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import { Nav, NavLink, NavLogo, NavMenu } from './navbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavMenu>
+                    <NavLogo>
+                        CSU - CS415
+                    </NavLogo>
+                    <NavLink to="/">
+                        Home
+                    </NavLink>
+                    <NavLink to="/workers">
+                        Workers
+                    </NavLink>
+                    <NavLink to="/projects">
+                        Projects
+                    </NavLink>
+                    <NavLink to="/qualifications">
+                        Qualifications
+                    </NavLink>
+                    <NavLink to="/assign">
+                        Assign
+                    </NavLink>
+                    <NavLink to="/about">
+                        About
+                    </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
